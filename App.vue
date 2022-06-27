442a20a56c3c04b49c6c1d7970d3020a32205bda
elena_tapean <105709367+Elena-Tapean@users.noreply.github.com>
2022-06-27 20:15:14 +0300

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <nav>
+      <router-link to="/">Homepage</router-link> |
+      <router-link to="/about">About Me</router-link> |
+      <router-link to="/contact">Contact</router-link>
+    </nav>
+    <router-view/>
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: black;
+}
+
+nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: black;
+
+    &.router-link-exact-active {
+      color: crimson;
+    }
+  }
+}
+</style>
